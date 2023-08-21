0354b1516b5b6bfa054659e813829c82a7e2ae44
Daniel Ramadan <44514691+DrEureka@users.noreply.github.com>
2023-08-21 19:25:41 -0300

    AVANCES de variables

--- app/src/main/java/com/drdev/andoridmaster/Variables.kt
+++ app/src/main/java/com/drdev/andoridmaster/Variables.kt
@@ -1,9 +1,57 @@
 package com.drdev.andoridmaster
 
 fun main(){
-    print("Variables")
+    print("Variables ")
     /**
-     * Variables
+     * Variables numericas
      */
+
+
+    //todo: creo una variable tipo int con los ":" me permite ingresar el tipo de variable que va ser
+    // Esto funciona porque kotlin es de programacion tipado fuerte entonces hay que aplicar el formato correcto para el tipo correcto.
+    // SOPORTA INT HASTA: -2,147,438,647 a 2,147,483,647
+    val intEjemplo:Int = 30
+
+    //todo: Long soporta hasta: -9,223,372,036,854,775,807 a 9,223,372,036,854,775,807
+    val longEjemplo:Long = 30
+
+    //todo: FLOAT soporta hasta:
+    var floatEjemplo:Float = 10.223F
+
+    //TODO: Doble
+    var dobleEjemplo:Double = 325.01212
+
+
+    /**
+     * Variables alfanumericas
+     */
+
+    //todo: Char toma solamente un caracter
+
+    var charEjemplo1:Char = 'a'
+    var charEjemplo2:Char = '2'
+    var charEjemplo3:Char = '@'
+
+
+    //todo: Creo una variable tipo string, no tiene limite
+    val miNombre = "Daniel R."
+
+    /**
+     * Variables boolean
+     */
+
+    //todo: permite ingresar true o false
+    var booleanEjemplo1:Boolean = true
+    var booleanEjemplo2:Boolean = false
+    var booleanEjemplo3:Boolean = true
+
+
+    /**
+     * invoco la variable
+     */
+print(floatEjemplo)
+
+
+
 
 }
