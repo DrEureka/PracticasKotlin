1f362a86f42c3222a5f9c755d95aba4e928196e1
Daniel Ramadan <44514691+DrEureka@users.noreply.github.com>
2023-08-21 19:49:37 -0300

    Variables y formatos

--- app/src/main/java/com/drdev/andoridmaster/Variables.kt
+++ app/src/main/java/com/drdev/andoridmaster/Variables.kt
@@ -11,6 +11,7 @@ fun main(){
     // Esto funciona porque kotlin es de programacion tipado fuerte entonces hay que aplicar el formato correcto para el tipo correcto.
     // SOPORTA INT HASTA: -2,147,438,647 a 2,147,483,647
     val intEjemplo:Int = 30
+    var intEjemplo2:Int = 25
 
     //todo: Long soporta hasta: -9,223,372,036,854,775,807 a 9,223,372,036,854,775,807
     val longEjemplo:Long = 30
@@ -31,10 +32,12 @@ fun main(){
     var charEjemplo1:Char = 'a'
     var charEjemplo2:Char = '2'
     var charEjemplo3:Char = '@'
+    val charEjemplo4:Char = '!'
 
 
     //todo: Creo una variable tipo string, no tiene limite
     val miNombre = "Daniel R."
+    val saludos:String = "Desde Argentina"
 
     /**
      * Variables boolean
@@ -49,8 +52,40 @@ fun main(){
     /**
      * invoco la variable
      */
-println(floatEjemplo)
+//println(floatEjemplo)
 
+    /**
+     * Aritmetica
+     */
+
+//    println("Sumar: ")
+//    println(intEjemplo + intEjemplo2)
+//
+//    println("Resta: ")
+//    println(intEjemplo - intEjemplo2)
+//
+//    println("Division: ")
+//    println(intEjemplo / intEjemplo2)
+//
+//    println("Multipl: ")
+//    println(intEjemplo * intEjemplo2)
+//
+//    println("Modulo: ")
+//    println(intEjemplo % intEjemplo2)
+
+    /**
+     * Variables concanetadas
+     */
+
+    var stringConcatenada:String = "hola"
+    println(stringConcatenada)
+    println("Hola, $miNombre $saludos")
+
+    /**
+     * Variables transformadas
+     */
+    //Todo: hay varios tipos de transformadores, chequear con ".toXXXXXX" elegir el formato correcto
+    val ejemploString:String = intEjemplo.toString()
 
 
 
